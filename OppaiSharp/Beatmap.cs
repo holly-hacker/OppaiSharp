c4ead59804aba454d25d2fc09983ebbbb2ea8f56
Piotrekol <nara1011@gmail.com>
2018-02-16 00:35:33 +0100

    Add Ability to only process part of beatmap

--- OppaiSharp/Beatmap.cs
+++ OppaiSharp/Beatmap.cs
@@ -10,7 +10,7 @@ namespace OppaiSharp
         public int FormatVersion { get; internal set; }
         public GameMode Mode { get; internal set; }
         public string Title { get; internal set; }
-        public string TitleUnicode  { get; internal set; }
+        public string TitleUnicode { get; internal set; }
         public string Artist { get; internal set; }
         public string ArtistUnicode { get; internal set; }
 
@@ -20,19 +20,117 @@ namespace OppaiSharp
         /// <summary>Difficulty name</summary>
         public string Version { get; internal set; }
 
-        public int CountCircles { get; set; }
-        public int CountSliders { get; set; }
-        public int CountSpinners { get; set; }
+        private int _allCountCircles;
+        private int _allCountSliders;
+        private int _allCountSpinners;
+        private int _cutCountCircles;
+        private int _cutCountSliders;
+        private int _cutCountSpinners;
+
+        public int CountCircles
+        {
+            get
+            {
+                if (Cutting)
+                    return _cutCountCircles;
+                return _allCountCircles;
+            }
+            set { _allCountCircles = value; }
+        }
+
+        public int CountSliders
+        {
+            get
+            {
+                if (Cutting)
+                    return _cutCountSliders;
+                return _allCountSliders;
+            }
+            set { _allCountSliders = value; }
+        }
+
+        public int CountSpinners
+        {
+            get
+            {
+                if (Cutting)
+                    return _cutCountSpinners;
+                return _allCountSpinners;
+            }
+            set { _allCountSpinners = value; }
+        }
+
         public float HP { get; set; } = 5f;
         public float CS { get; set; } = 5f;
         public float OD { get; set; } = 5f;
         public float AR { get; set; } = 5f;
         public float SliderVelocity { get; set; } = 1f;
         public float TickRate { get; set; } = 1f;
+        public bool Cutting { get; private set; } = false;
+        private List<HitObject> _allObjects = new List<HitObject>(512);
+        private List<HitObject> CutObjects { get; set; }
+        public List<HitObject> Objects
+        {
+            get
+            {
+                if (Cutting)
+                    return CutObjects;
+                return _allObjects;
+            }
+            set { _allObjects = value; }
+        }
+        private List<Timing> AllTimingPoints { get; set; } = new List<Timing>(32);
+        private List<Timing> CutTimingPoints { get; set; } = new List<Timing>(32);
 
-        public List<HitObject> Objects { get; } = new List<HitObject>(512);
-        public List<Timing> TimingPoints { get; } = new List<Timing>(32);
+        public List<Timing> TimingPoints
+        {
+            get
+            {
+                if (Cutting)
+                    return CutTimingPoints;
+                return AllTimingPoints;
+            }
+        }
 
+        public void Cut(int endTime)
+        {
+            if (endTime <= 0)
+                throw new ArgumentException(nameof(endTime));
+            Cutting = true;
+            CutObjects = new List<HitObject>();
+            foreach (var o in _allObjects)
+            {
+                if (o.Time < endTime)
+                    CutObjects.Add(o);
+                else//Assuming chronological order
+                    break;
+            }
+            CutTimingPoints = new List<Timing>();
+            foreach (var t in AllTimingPoints)
+            {
+                if (t.Time < endTime)
+                    CutTimingPoints.Add(t);
+                else//Assuming chronological order
+                    break;
+            }
+            _cutCountCircles = 0;
+            _cutCountSpinners = 0;
+            _cutCountSliders = 0;
+            foreach (var o in CutObjects)
+            {
+                if (o.Type == HitObjectType.Circle)
+                    _cutCountCircles++;
+                else if (o.Type == HitObjectType.Slider)
+                    _cutCountSliders++;
+                else if (o.Type == HitObjectType.Spinner)
+                    _cutCountSpinners++;
+            }
+        }
+
+        public void ResetCut()
+        {
+            Cutting = false;
+        }
         public static Beatmap Read(StreamReader reader) => Parser.Read(reader);
 
         internal Beatmap() { }
